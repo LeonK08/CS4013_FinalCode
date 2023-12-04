dc57ddacae1acecb67f36c1fb316a13f12c9c859
LM-22347526 <22347526@studentmail.ul.ie>
2023-12-04 00:19:11 +0000

    Added Javadoc to Faculty

--- Faculty.java
+++ Faculty.java
@@ -5,16 +5,29 @@ public class Faculty {
     private String name;
     private ArrayList<String> modulesTaught;
 
+    /**
+     * Constructs a faculty member with the given ID and name
+     * @param facultyId ID of the faculty member
+     * @param name Name of the faculty member
+     */
     public Faculty(String facultyId, String name) {
         this.facultyId = facultyId;
         this.name = name;
         this.modulesTaught = new ArrayList<String>();
     }
 
+    /**
+     * Adds a Module to the list of modules taught by the faculty member
+     * @param module The Module object to add to modulesTaught
+     */
     public void addModule(String module) {
         modulesTaught.add(module);
     }
 
+    /**
+     * Removes a module from the list of modules taught by the faculty member. If the faculty member is already not teaching that module, displays a message saying so
+     * @param module The Module to remove from modulesTaught
+     */
     public void removeModule(String module) {
         if (modulesTaught.contains(module)) {
             modulesTaught.remove(module);
@@ -23,11 +36,16 @@ public class Faculty {
         }
     }
 
+    /**
+     * Returns string formatted details of the faculty member (ID, name and modules taught)
+     */
     public void getFacultyDetails() {
         System.out.println("Faculty ID: " + facultyId);
         System.out.println("Name: " + name);
         System.out.println("Modules taught: " + modulesTaught);
     }
+
+    // Getters
     public String getFacultyId(){
         return facultyId;
     }
